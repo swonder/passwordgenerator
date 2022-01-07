31b78af3c6dc1567b4b873be695c4c61b8204ce1
Shawn Wonder <shawn.wonder@printerlogic.com>
2022-01-06 17:31:48 -0700

    Refactor main file

--- server.go
+++ server.go
@@ -1,130 +1,86 @@
 package main
 
 import (
-  //"log"
-  "strconv"
-  "net/http"
-  "math/rand"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 )
 
-import "github.com/go-martini/martini"
-import "github.com/martini-contrib/render"
-
-
-type Response struct {
-  Number string
-  Size string
-  Specchars string
-  Ucchars string
-  Passwords []string
-}
-
-var ucletters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-var lcletters = []rune("abcdefghijklmnopqrstuvwxyz")
-var ucchars =   []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*.-|")
-var lcchars =   []rune("abcdefghijklmnopqrstuvwxyz!@#$%^&*.-|")
-
-//Create a random length string
-func randString(n int, special string, uc string) string {
-    b := make([]rune, n)
-    if special == "checked" && uc == "checked" {
-      for i := range b {
-          b[i] = ucchars[rand.Intn(len(ucchars))]
-      }
-    } else if special != "checked" && uc == "checked" {
-      for i := range b {
-          b[i] = ucletters[rand.Intn(len(ucletters))]
-      }
-    } else if special == "checked" && uc != "checked" {
-      for i := range b {
-          b[i] = lcchars[rand.Intn(len(lcchars))]
-      }
-    } else {
-      for i := range b {
-          b[i] = lcletters[rand.Intn(len(lcletters))]
-      }
-    }
-
-    return string(b)
-}
-
 func main() {
-  var pwds []string
+	response := Response{
+		NumberPasswords: 1,
+		PasswordSize:    14,
+		SpecChars:       "checked",
+		UcChars:         "checked",
+		Passwords:       []string{},
+	}
 
-  response := Response {
-    Number:             "1",
-    Size:               "14",
-    Specchars:          "checked",
-    Ucchars:            "checked",
-    Passwords:          []string{},
-  }
+	m := martini.Classic()
 
-  m := martini.Classic()
-  //Include css, js, etc
-  m.Use(martini.Static("assets"))
-  m.Use(render.Renderer())
+	m.Use(martini.Static("assets"))
+	m.Use(render.Renderer())
 
-  // render.Options{Layout: "layout",
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "home", response)
+		response.Passwords = []string{}
+	})
 
-  //Root route
-  m.Get("/", func(r render.Render) {
-       r.HTML(200, "home", response)
-       response.Passwords = []string{}
-  })
+	m.Post("/", func(req *http.Request, r render.Render) {
+		gatherInput(req, &response)
 
-  //Posting to a form in root route
-  m.Post("/" ,func(req *http.Request, r render.Render) {
-      pwds = []string{}
-      formnumber := req.FormValue("number")
-      formsize := req.FormValue("size")
-      formspecchars := req.FormValue("specchars")
-      formucchars := req.FormValue("ucchars")
-      if formspecchars == "on" {
-        formspecchars = "checked"
-      }
-      if formucchars == "on" {
-        formucchars = "checked"
-      }
+		validateInput(&response)
 
-      if len(formsize) > 0 {
-        number, err := strconv.Atoi(formnumber)
-        size, err := strconv.Atoi(formsize)
-        if number > 0 && number < 100 {
-          if err == nil {
-            if size > 0 && size <= 75 {
-              //Loop through and create 'number' number of passwords
-              for i := 0; i < number; i++ {
-                pwds = append(pwds, randString(size, formspecchars, formucchars))
-                //strconv.Itoa(i+1) + ":  " +
-              }
+		r.Redirect("/")
+	})
 
-              response = Response {
-                Number:             strconv.Itoa(number),
-                Size:               strconv.Itoa(size),
-                Specchars:          formspecchars,
-                Ucchars:            formucchars,
-                Passwords:          pwds,
-              }
+	m.Run()
+}
 
-            } else {
-              pwds = append([]string{}, "Password length must be greater than 0 and less than 75 characters ")
-              response.Passwords = pwds
-            }
-          } else {
-            pwds = append([]string{}, "Password length must be a number")
-            response.Passwords = pwds
-          }
-        } else {
-          pwds = append([]string{}, "Number of passwords must be greater than 0 and less or equal to 100")
-          response.Passwords = pwds
-        }
+func gatherInput(req *http.Request, res *Response) {
+	numberPasswords, err := strconv.Atoi(req.FormValue("number"))
+	if err != nil {
+		log.Print("Unable to convert form number of passwords variable to a number")
+	}
+	res.NumberPasswords = numberPasswords
 
-      } else {
-        pwds = append([]string{}, "Please make sure number of passwords and password lengths are set")
-        response.Passwords = pwds
-      }
-      r.Redirect("/")
-  })
+	passwordSize, err := strconv.Atoi(req.FormValue("size"))
+	if err != nil {
+		log.Print("Unable to convert form password size input to a number")
+	}
+	res.PasswordSize = passwordSize
 
-  m.Run()
+	formSpecChars := req.FormValue("specchars")
+	res.SpecChars = ""
+	if formSpecChars == "on" {
+		res.SpecChars = "checked"
+	}
+
+	formUcChars := req.FormValue("ucchars")
+	res.UcChars = ""
+	if formUcChars == "on" {
+		res.UcChars = "checked"
+	}
+}
+
+func validateInput(res *Response) {
+	validationError := false
+
+	if res.NumberPasswords < 1 || res.NumberPasswords > 100 {
+		validationError = true
+		res.Passwords = append(res.Passwords, "Number of passwords must be a number more than 0 and no more than 100")
+	}
+
+	if res.PasswordSize < 1 || res.PasswordSize > 75 {
+		validationError = true
+		res.Passwords = append(res.Passwords, "Password lengths must be a number more than 0 and no more than 75")
+	}
+
+	if !validationError {
+		for i := 0; i < res.NumberPasswords; i++ {
+			res.Passwords = append(res.Passwords, generateRandomString(res.PasswordSize, res.SpecChars, res.UcChars))
+		}
+	}
 }
